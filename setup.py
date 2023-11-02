a7a8213035e0625258c47495bd289eeee5fa3fcb
Simon Willison <swillison@gmail.com>
2023-11-02 13:58:55 -0700

    Applied Black

--- setup.py
+++ setup.py
@@ -27,15 +27,13 @@ setup(
     license="Apache License, Version 2.0",
     classifiers=[
         "Framework :: Datasette",
-        "License :: OSI Approved :: Apache Software License"
+        "License :: OSI Approved :: Apache Software License",
     ],
     version=VERSION,
     packages=["datasette_enrichments"],
     entry_points={"datasette": ["enrichments = datasette_enrichments"]},
     install_requires=["datasette"],
     extras_require={"test": ["pytest", "pytest-asyncio"]},
-    package_data={
-        "datasette_enrichments": ["templates/*"]
-    },
+    package_data={"datasette_enrichments": ["templates/*"]},
     python_requires=">=3.7",
 )
