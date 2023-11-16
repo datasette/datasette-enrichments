986288e6f9ca43077ec61f7b08f281f2ba0ca0a8
Simon Willison <swillison@gmail.com>
2023-11-16 09:18:47 -0800

    Initial enrichment test

---
+++ tests/conftest.py
@@ -0,0 +1,63 @@
+from datasette.database import Database
+from typing import List
+from wtforms import Form, SelectField
+from wtforms.widgets import ListWidget, CheckboxInput
+import pytest
+from datasette.plugins import pm
+from datasette import hookimpl
+
+
+class MultiCheckboxField(SelectField):
+    widget = ListWidget(prefix_label=False)
+    option_widget = CheckboxInput()
+
+
+@pytest.fixture(autouse=True)
+def load_uppercase_plugin():
+    from datasette_enrichments import Enrichment
+
+    class UppercaseDemo(Enrichment):
+        name = "Convert to uppercase"
+        slug = "uppercasedemo"
+        description = "Convert selected columns to uppercase"
+        runs_in_process = True
+
+        async def get_config_form(self, db, table):
+            choices = [(col, col) for col in await db.table_columns(table)]
+
+            class ConfigForm(Form):
+                columns = MultiCheckboxField("Columns", choices=choices)
+
+            return ConfigForm
+
+        async def enrich_batch(
+            self,
+            db: Database,
+            table: str,
+            rows: List[dict],
+            pks: List[str],
+            config: dict,
+            job_id: int,
+        ):
+            columns = config.get("columns") or []
+            if not columns:
+                return
+            wheres = " and ".join('"{}" = ?'.format(pk) for pk in pks)
+            sets = ", ".join('"{}" = upper("{}")'.format(col, col) for col in columns)
+            params = [[row[pk] for pk in pks] for row in rows]
+            await db.execute_write_many(
+                "update [{}] set {} where {}".format(table, sets, wheres), params
+            )
+
+    class UppercasePlugin:
+        __name__ = "UppercasePlugin"
+
+        @hookimpl
+        def register_enrichments(self):
+            return [UppercaseDemo()]
+
+    pm.register(UppercasePlugin(), name="undo_uppercase")
+    try:
+        yield
+    finally:
+        pm.unregister(name="undo_uppercase")
--- tests/test_enrichments.py
+++ tests/test_enrichments.py
@@ -1,11 +1,42 @@
 from datasette.app import Datasette
 import pytest
+import sqlite3
 
 
 @pytest.mark.asyncio
-async def test_plugin_is_installed():
-    datasette = Datasette([], memory=True)
-    response = await datasette.client.get("/-/plugins.json")
-    assert response.status_code == 200
-    installed_plugins = {p["name"] for p in response.json()}
-    assert "datasette-enrichments" in installed_plugins
+async def test_uppercase_plugin(tmpdir):
+    data = str(tmpdir / "data.db")
+    db = sqlite3.connect(data)
+    with db:
+        db.execute("create table t (id integer primary key, s text)")
+        db.execute("insert into t (s) values ('hello')")
+        db.execute("insert into t (s) values ('goodbye')")
+    datasette = Datasette([data])
+
+    cookies = {"ds_actor": datasette.sign({"a": {"id": "root"}}, "actor")}
+    response1 = await datasette.client.get("/-/enrich/data/t", cookies=cookies)
+    assert (
+        '<a href="/-/enrich/data/t/uppercasedemo?_enrichment=uppercasedemo">Convert to uppercase</a>'
+        in response1.text
+    )
+
+    response2 = await datasette.client.get(
+        "/-/enrich/data/t/uppercasedemo?_enrichment=uppercasedemo", cookies=cookies
+    )
+    assert "<h2>Convert to uppercase</h2>" in response2.text
+
+    # Now try and run it
+    csrftoken = response2.cookies["ds_csrftoken"]
+    cookies["ds_csrftoken"] = csrftoken
+
+    response3 = await datasette.client.post(
+        "/-/enrich/data/t/uppercasedemo?_enrichment=uppercasedemo",
+        cookies=cookies,
+        data={"columns": "s", "csrftoken": csrftoken},
+    )
+    assert response3.status_code == 302
+    assert response3.headers["location"] == "/data/t"
+    # It should be queued up
+    assert db.execute(
+        "select status, enrichment, database_name, table_name, config from _enrichment_jobs"
+    ).fetchall() == [("p", "uppercasedemo", "data", "t", '{"columns": ["s"]}')]
